1b3ada3167317c32ae2056b4d702ec870f23771e
nightlighttw <david97099@gmail.com>
2023-08-15 12:01:04 +0800

    branch

---
+++ examples/beta/py/new/target.py
@@ -0,0 +1,9 @@
+# START_CUSTOM_CODE
+dataset = "tf.keras.datasets.cifar10"
+# END_CUSTOM_CODE
+
+def main(x):
+    {
+        print(x)
+    }
+main(dataset)
---
+++ examples/beta/py/new/test.py
@@ -0,0 +1,4 @@
+import sys
+if len(sys.argv) < 2:
+    print('no argument')
+print("Got arguments: ", sys.argv)
